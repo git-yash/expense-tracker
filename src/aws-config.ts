547893704bf43a627365f0e0d3286b904120baa0
Yash Shah <62358049+git-yash@users.noreply.github.com>
2025-01-03 23:26:45 -0600

    removed dotenv package, replaced process with import.meta

--- src/aws-config.ts
+++ src/aws-config.ts
@@ -1,12 +1,10 @@
-import * as process from "node:process";
-
 export const awsConfig = {
-    aws_project_region: process.env.REACT_APP_REGION,
-    aws_cognito_identity_pool_id: process.env.REACT_APP_IDENTITY_POOL_ID,
-    aws_cognito_region: process.env.REACT_APP_REGION,
-    aws_user_pools_id: process.env.REACT_APP_USER_POOL_ID,
-    aws_user_pools_web_client_id: process.env.REACT_APP_USER_POOL_CLIENT_ID,
-    aws_appsync_graphqlEndpoint: process.env.REACT_APP_APP_SYNC_URL,
-    aws_appsync_region: process.env.REACT_APP_REGION,
-    aws_appsync_authenticationType: process.env.REACT_APP_AUTHENTICATION_TYPE,
+    aws_project_region: import.meta.env.VITE_REGION,
+    aws_cognito_identity_pool_id: import.meta.env.VITE_IDENTITY_POOL_ID,
+    aws_cognito_region: import.meta.env.VITE_REGION,
+    aws_user_pools_id: import.meta.env.VITE_USER_POOL_ID,
+    aws_user_pools_web_client_id: import.meta.env.VITE_USER_POOL_CLIENT_ID,
+    aws_appsync_graphqlEndpoint: import.meta.env.VITE_APP_SYNC_URL,
+    aws_appsync_region: import.meta.env.VITE_REGION,
+    aws_appsync_authenticationType: import.meta.env.VITE_AUTHENTICATION_TYPE,
 };
